9037c848c00b5e0eef5a7ed090c43ac4f6e3693e
Bhaskar Mookerji <mookerji@gmail.com>
2013-05-29 12:53:01 -0700

    Removed old setup.py cruft.

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
  #! /usr/bin/env python
 
- # Include option for ignore cython distutils.
-
 from distutils.core import setup
-# from distutils.extension import Extension
-# from Cython.Distutils import build_ext
 
 setup(name='diodberg',
       version='0.1',
@@ -13,13 +9,4 @@ setup(name='diodberg',
       author_email='mookerji@alum.mit.edu',
       url='http://spin-one.org',
       packages=['diodberg'],
-      # cmdclass = {'build_ext': build_ext},
-      # ext_modules = [Extension("dmxbuffer", ["ola_client/dmxbuffer.pyx"],
-      #                          libraries=["ola", "olacommon", "protobuf"], language="c++"),
-      #                Extension("olaclient", ["ola_client/olaclient.pyx"],
-      #                          libraries=["ola", "olacommon", "protobuf"], language="c++"),
-      #                Extension("olaclientwrapper", ["ola_client/olaclientwrapper.pyx"],
-      #                          libraries=["ola", "olacommon", "protobuf"], language="c++"),
-      #                Extension("streamingclient", ["ola_client/streamingclient.pyx"],
-      #                          libraries=["ola", "olacommon", "protobuf"], language="c++")]
      )
